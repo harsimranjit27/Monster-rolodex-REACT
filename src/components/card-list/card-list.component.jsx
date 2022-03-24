f4493f1e865c706458593560a9ce1b719e0f0664
harsimranjit27 <harsimran2731@gmail.com>
2022-03-24 14:25:08 +0530

    added image, name, email to app

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,25 @@
 import { Component } from 'react'
+import './card-list.styles.css'
+import './card.styles.css'
 
 class ClassList extends Component {
 
     render(props) {
         
         const {monsters} = this.props;
-        return (
-            <div>{monsters.map(monster => {
-                    return (<h1 key={monster.id}> {monster.name} </h1>);
-                })}
-            </div>
-        )
+        
+        return <div className="card-list">
+            {monsters.map(monster => {
+                const {name, id, email} = monster;
+                return (
+                    <div className="card-container" key={id}>
+                        <img src={`https://robohash.org/${id}/set=set2&size=100x100`} alt={`monster ${name}`}/>
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+                )
+            })}
+        </div>
     }
 }
 export default ClassList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import './search-box.styles.css'
 
 class SearchBox extends Component {
     render(props) {
@@ -6,6 +7,7 @@ class SearchBox extends Component {
         return (
             <div>
                 <input 
+                    className={`search-box ${this.props.className}`}
                     type={this.props.className}
                     placeholder={placeholder}
                     onChange = {onChangeHandler}
